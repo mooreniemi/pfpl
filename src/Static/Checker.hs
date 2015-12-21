f2fb6e0cc5bc16e705a44e6d378781bffb05fc52
Alex Moore-Niemi <moore.niemi@gmail.com>
2015-12-21 00:00:14 -0500

    working through cases

--- src/Static/Checker.hs
+++ src/Static/Checker.hs
@@ -1,20 +1,47 @@
 module Static.Checker where
+import qualified Data.Map.Strict as Map
 
 data EType = TNum
            | TStr
            deriving (Show, Eq)
 
-data EExp = ENum Int
+data EExp = EId String
+          | ENum Int
           | EStr String
           | EAdd EExp EExp
           | EMult EExp EExp
           | ECon EExp EExp
           | ELen EExp
-          | EDef EExp String EExp
+          | EDef { value :: EExp
+                 , identifier :: String
+                 , body :: EExp
+                 }
           deriving (Show, Eq)
 
-check :: EExp -> EType
-check expression = case expression of
-  ENum _ -> TNum
-  _ -> undefined
+type TypeEnv = Map.Map String EType
 
+check' :: EExp -> EType
+check' = check Map.empty
+
+check :: TypeEnv -> EExp -> EType
+check typeEnv expression = case expression of
+  EId stringId -> case Map.lookup stringId typeEnv of
+    Just typeValue -> typeValue
+    Nothing -> undefined
+  ENum _ -> TNum
+  EStr _ -> TStr
+  EAdd (ENum _) (ENum _) -> TNum
+  EMult (ENum _) (ENum _) -> TNum
+  ECon (EStr _) (EStr _) -> TStr
+  ELen (EStr _) -> TNum
+  ELen expr@(EId stringId) -> let typeOfExpr = check typeEnv expr
+                              in case typeOfExpr of
+                                  TStr -> TNum
+                                  e -> error' "Can only take length of strings." e
+  EDef expr1 stringId expr2 -> let typeOfExpr1 = check typeEnv expr1
+                                   typeEnv' = Map.insert stringId typeOfExpr1 typeEnv
+                               in check typeEnv' expr2
+  e -> error' "Failed to match:" e
+
+error' :: Show a => String -> a -> b
+error' message errorValue = error $ message ++ show errorValue
--- test/Static/CheckerSpec.hs
+++ test/Static/CheckerSpec.hs
@@ -1,14 +1,30 @@
 module Static.CheckerSpec where
 import           Static.Checker
 import           Test.Hspec
-                 
+
 spec :: Spec
-spec = do
-  describe "check" $ do
-    it "should return TNum for ENum expression" $ do
-      check (ENum 1) `shouldBe` TNum
+spec =
+  describe "check'" $ do
+    it "should return TNum for ENum expression" $
+      check' (ENum 1) `shouldBe` TNum
 
+    it "should return TStr for EStr expression" $
+      check' (EStr "string") `shouldBe` TStr
 
+    it "should return TNum for EAdd" $
+       check' (EAdd (ENum 1) (ENum 1)) `shouldBe` TNum
 
+    it "should return TNum for EMult" $
+       check' (EMult (ENum 1) (ENum 1)) `shouldBe` TNum
 
+    it "should return TStr for ECon" $
+       check' (ECon (EStr "string") (EStr "string")) `shouldBe` TStr
 
+    it "should return TStr for ELen" $
+       check' (ELen (EStr "string")) `shouldBe` TNum
+
+    it "should return TStr when e1 is EStr and x is e2" $
+       check' (EDef (EStr "string") "x" (EId "x")) `shouldBe` TStr
+
+    it "should return TNum when e1 is EStr and e2 is ELen of x" $
+       check' (EDef (EStr "string") "x" (ELen (EId "x"))) `shouldBe` TNum
